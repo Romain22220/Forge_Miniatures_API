8dddb6bece7405d33d3672a665005ce8f53c6bcd
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-06-08 22:30:05 +0200

    Création projet

---
+++ src/main/java/com/forge_miniature/ForgeMiniatureApiApplication.java
@@ -0,0 +1,13 @@
+package com.forge_miniature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgeMiniatureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgeMiniatureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forge_miniature/ForgeMiniatureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forge_miniature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgeMiniatureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
