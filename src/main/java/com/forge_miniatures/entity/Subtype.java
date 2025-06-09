e6767dd8ddf2e6c7fa83e38497dbbdb06a59564d
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-06-09 14:22:52 +0200

    Correction Type / Subtype

--- src/main/java/com/forge_miniatures/entity/Subtype.java
+++ src/main/java/com/forge_miniatures/entity/Subtype.java
@@ -1,7 +1,26 @@
 package com.forge_miniatures.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="t1m_subtype_sbp")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Subtype {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="idt_subtype_sbp")
     private Long id;
+
+    @Column(name="nom_subtype")
     private String nom;
+
+    @ManyToOne
+    @JoinColumn(name = "t1m_idt_type", referencedColumnName = "idt_type")
     private Type type;
 }
--- src/main/java/com/forge_miniatures/entity/Type.java
+++ src/main/java/com/forge_miniatures/entity/Type.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Table(name = "t1m_type_tpe")
 @Data
@@ -18,6 +20,9 @@ public class Type {
 
     @Column(name="nom")
     private String nom;
+
+    @OneToMany(mappedBy = "type", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Subtype> subtypes;
 
 
 }
