6e89d6075820319a2b9a2e7a4635907c00037855
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-06-09 13:38:11 +0200

    Ajout ArticleDTO

--- src/main/java/com/forge_miniatures/dto/ArticleDTO.java
+++ src/main/java/com/forge_miniatures/dto/ArticleDTO.java
@@ -1,4 +1,26 @@
 package com.forge_miniatures.dto;
 
+import com.forge_miniatures.entity.Article_Image;
+import com.forge_miniatures.entity.Reference;
+import com.forge_miniatures.entity.Scale;
+import com.forge_miniatures.entity.Type;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ArticleDTO {
+    private Long id;
+    private String nom;
+    private String description;
+    private List<Article_Image> images;
+    private double prix;
+    private int quantite;
+    private Type type;
+    private Scale scale;
+    private Reference reference;
 }
--- src/main/java/com/forge_miniatures/entity/Article.java
+++ src/main/java/com/forge_miniatures/entity/Article.java
@@ -1,9 +1,12 @@
 package com.forge_miniatures.entity;
 
+import java.util.List;
+
 public class Article {
     private Long id;
     private String nom;
     private String description;
+    private List<Article_Image> images;
     private double prix;
     private int quantite;
     private Type type;
