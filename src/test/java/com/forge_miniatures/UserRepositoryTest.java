8fff5bd526b663e3a6dd79d01b6c32514521a1d5
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-06-20 15:38:41 +0200

    Ajout Test UserRepositoryTest

---
+++ src/test/java/com/forge_miniatures/UserRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.forge_miniatures;
+
+import com.forge_miniatures.entity.User;
+import com.forge_miniatures.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testSaveUser(){
+        User user = new User(null, "Dupont", "Jean", "jean123", "0612345678", "jean@mail.com", "pass123", "12 rue de Paris");
+        User result = userRepository.save(user);
+        Assertions.assertNotNull(result.getId());
+
+    }
+}
