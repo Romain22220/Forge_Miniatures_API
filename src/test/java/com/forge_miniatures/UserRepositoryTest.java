f95e607275162d0dbc2a24f1dca3ffc4e3ace8ed
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-06-27 22:57:17 +0200

    Ajout Test UserRepositoryTest

--- src/test/java/com/forge_miniatures/UserRepositoryTest.java
+++ src/test/java/com/forge_miniatures/UserRepositoryTest.java
@@ -16,19 +16,80 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    /*
+        Création d'un User Jean avec ses caractéristiques
+     */
+        User Jean = new User(null, "Dupont", "Jean", "jean123", "jean@mail.com", "0612345678", "12 rue de Paris", "pass123");
+
+    /*
+        Test permettant de voir si un User est bien sauvegardé
+     */
     @Test
     public void testSaveUser(){
-        User user = new User(null, "Dupont", "Jean", "jean123", "0612345678", "jean@mail.com", "pass123", "12 rue de Paris");
-        User result = userRepository.save(user);
+        User result = userRepository.save(Jean);
         Assertions.assertNotNull(result.getId());
     }
 
+    /*
+        Test permettant de trouver un User existant par son Id
+     */
     @Test
     public void testFindUserById(){
-        User user = new User(null, "Dupont", "Jean", "jean123", "0612345678", "jean@mail.com", "pass123", "12 rue de Paris");
-        userRepository.save(user);
-        User result = userRepository.findById(user.getId());
-        System.out.println("Le user a pour ID :" + result.getId());
+        User savedUser = userRepository.save(Jean);
+        Long id = savedUser.getId();
+        User result = userRepository.findById(id);
         Assertions.assertNotNull(result);
+    }
+
+    /*
+        Test permettant de vérifier qu'on ne trouve pas un User par un Id non existant
+     */
+    @Test
+    public void testFindUserByIdNotExistant(){
+        Long id = 5L;
+        User result = userRepository.findById(id);
+        Assertions.assertNull(result);
+
+    }
+
+    /*
+        Test permettant de trouver un User existant par son adresse email
+     */
+    @Test
+    public void testFindUserByEmail(){
+        userRepository.save(Jean);
+        User result = userRepository.findByEmail("jean@mail.com");
+        Assertions.assertNotNull(result);
+    }
+
+    /*
+        Test permettant de vérifier qu'une adresse email non présente génère un null
+     */
+    @Test
+    public void testFindUserByEmailNotExistant(){
+        User result = userRepository.findByEmail("coco@coco.fr");
+        Assertions.assertNull(result);
+    }
+
+    /*
+        Test de la fonction FindByPseudo :
+        > Test avec un pseudo existant
+     */
+    @Test
+    public void testFindUserByPseudoExistant(){
+        userRepository.save(Jean);
+        User result = userRepository.findByPseudo("jean123");
+        Assertions.assertNotNull(result);
+    }
+
+    /*
+        Test de la fonction FindByPseudo :
+        > Test avec un pseudo non existant
+     */
+    @Test
+    public void testFindUserByPseudoNotExistant(){
+        userRepository.save(Jean);
+        User result = userRepository.findByPseudo("jean124");
+        Assertions.assertNull(result);
     }
 }
