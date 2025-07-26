cb1fe144ddd6881c4345f9aaa94fd5728c07737b
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-07-26 19:17:21 +0200

    Ajout de l'interface et du service Scale

---
+++ src/main/java/com/forge_miniatures/service/ScaleService.java
@@ -0,0 +1,14 @@
+package com.forge_miniatures.service;
+
+import com.forge_miniatures.dto.ScaleDTO;
+import com.forge_miniatures.entity.Scale;
+
+import java.util.List;
+
+public interface ScaleService {
+    ScaleDTO createScale(ScaleDTO scaleDTO);
+    ScaleDTO updateScale(ScaleDTO scaleDTO);
+    ScaleDTO findScaleById(Long id);
+    List<ScaleDTO> getAllScales();
+    void deleteScaleById(Long id);
+}
---
+++ src/main/java/com/forge_miniatures/service/ScaleServiceImpl.java
@@ -0,0 +1,49 @@
+package com.forge_miniatures.service;
+
+import com.forge_miniatures.dto.ScaleDTO;
+import com.forge_miniatures.entity.Scale;
+import com.forge_miniatures.mapper.ScaleMapper;
+import com.forge_miniatures.repository.ScaleRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ScaleServiceImpl implements   ScaleService {
+    private final ScaleRepository scaleRepository;
+
+    @Override
+    public ScaleDTO createScale(ScaleDTO scaleDTO) {
+        Scale scale = ScaleMapper.toScale(scaleDTO);
+
+        return ScaleMapper.toScaleDTO(scaleRepository.save(scale));
+    }
+
+    @Override
+    public ScaleDTO updateScale(ScaleDTO scaleDTO) {
+        return null;
+    }
+
+    @Override
+    public ScaleDTO findScaleById(Long id) {
+        Scale scale = scaleRepository.findScaleById(id);
+        if(scale == null){
+            throw new EntityNotFoundException("Scale with "+ id + " not found. Please select a real scale. ");
+        }
+        return ScaleMapper.toScaleDTO(scale);
+    }
+
+    @Override
+    public List<ScaleDTO> getAllScales() {
+        return scaleRepository.findAll().stream().map(ScaleMapper::toScaleDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteScaleById(Long id) {
+        scaleRepository.deleteScaleById(id);
+    }
+}
