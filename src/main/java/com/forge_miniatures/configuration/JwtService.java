cf257a45cd2669ffd4d37c289a10769fd3ce1f4f
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-10-02 23:41:37 +0200

    Ajout : Ajout des JWT

---
+++ src/main/java/com/forge_miniatures/configuration/JwtService.java
@@ -0,0 +1,51 @@
+package com.forge_miniatures.configuration;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final long EXPIRATION = 1000 * 60 * 60;
+    private static final String  SECRET_TEST = "ton_super_secret_key_de_256bits_ici";
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_TEST.getBytes());
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
