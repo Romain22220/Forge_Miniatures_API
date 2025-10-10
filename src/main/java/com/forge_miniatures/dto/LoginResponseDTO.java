156d422701635030e5922e00c3ab62c3784ba926
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-10-10 10:30:11 +0200

    SUPPORT-12 : Création des DTO pour la connexion et la génération du token

---
+++ src/main/java/com/forge_miniatures/dto/LoginResponseDTO.java
@@ -0,0 +1,10 @@
+package com.forge_miniatures.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private String token;
+}
