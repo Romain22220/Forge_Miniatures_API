9db242c0ba164a7abca7d997a94b90bec3fb34ac
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-10-17 22:49:54 +0200

    SUPPORT-12 : FIX JWT configuration

---
+++ src/main/java/com/forge_miniatures/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.forge_miniatures.service;
+
+import com.forge_miniatures.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé : " + email));
+    }
+}
