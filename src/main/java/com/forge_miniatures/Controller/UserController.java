3e883d710149c43549d10b26565f5c652dbb5f16
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-10-17 22:47:13 +0200

    ADD : Endpoint de vérification d'email

--- src/main/java/com/forge_miniatures/Controller/UserController.java
+++ src/main/java/com/forge_miniatures/Controller/UserController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,5 +18,10 @@ public class UserController {
     @PostMapping("/create")
     public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<UserDTO> getUserByEmail(@RequestParam String email) {
+        return ResponseEntity.ok(userService.findUserByEmail(email));
     }
 }
